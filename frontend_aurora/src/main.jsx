4e56c483418b31ee62cc72d5563db0a633cb1b6f
DavidRamirez152 <davidf_ramirezm@soy.sena.edu.co>
2024-09-20 14:38:47 -0500

    BarraLateral
    Se crea la barra lateral que se usara en todas las ventanas

--- frontend_aurora/src/main.jsx
+++ frontend_aurora/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './home/login.css'
 import Login from './home/login.jsx'; 
+import LoginDavid from './Modulos/davidLogin/login.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <LoginDavid />
   </StrictMode>,
 );
